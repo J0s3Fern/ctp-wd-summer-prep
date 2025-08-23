3e58bef4b8b42fbe96a2623d7c3b63333c191930
J0s3Fern <josefernandez0066@gmail.com>
2025-08-23 02:30:24 +0000

    More CSS details and animation, added complete button, changed editing function to place edited habit back at its original position in the list.

--- habit-tracker/C11/J0s3Fern/src/main.js
+++ habit-tracker/C11/J0s3Fern/src/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('habit_form')
 const habits = []
+let editingIdx = null; // Track which habit is being edited
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -11,10 +12,14 @@ form.addEventListener('submit', (event) => {
         targetStreak: Number(data.get('target_streak'))
     }
 
-    habits.push(habit)
+    if (editingIdx !== null) {
+        habits.splice(editingIdx, 0, habit)
+        editingIdx = null
+    } else {
+        habits.push(habit)
+    }
 
-    console.log(JSON.stringify(habits))
-
+    form.reset()
     renderHabits(habits)
 })
 
@@ -23,11 +28,19 @@ const renderHabits = (habits) => {
 
     habitList.innerHTML = habits.map((habit, idx) => {
         return `
-            <li class="habit_card">
-                ${habit.name}, ${habit.targetStreak}
-                <button class="edit-btn" data-idx="${idx}">Edit</button>
-                <button class="delete-btn" data-idx="${idx}">Delete</button>
-            </li>
+            <li class="habit_card"> <div class="card_content">
+                <div class="displayed_name">${habit.name}</div>
+                <div class="displayed_streak">${habit.targetStreak} day streak</div>
+
+                <div>
+                    <button class="complete_button">Complete</button>
+                </div>
+
+                <div>
+                    <button class="edit-btn" data-idx="${idx}">Edit</button>
+                    <button class="delete-btn" data-idx="${idx}">Delete</button>
+                </div>
+            </div> </li>
         `
     }).join('\n')
 
@@ -51,7 +64,30 @@ const renderHabits = (habits) => {
             
             // Remove the habit from the list so it can be re-added on submit
             habits.splice(idx, 1)
+            editingIdx = idx;
+
             renderHabits(habits)
         })
     })
 }
+
+
+
+
+
+// TODO: Local storage
+
+const habitStorage = {
+  // TODO: Implement these methods
+  save(habits) {
+    window.localStorage.setItem('habits', JSON.stringify(habits))
+  },
+  
+  load() {
+    JSON.parse(localStorage.getItem('habits'))
+  },
+  
+  clear() {
+    localStorage.removeItem('habits')
+  }
+};
